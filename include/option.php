6b93c22c5b75ac10d99977725b4c2e37fa244984
tsp27 <tsp27@mail.ru>
2023-02-22 01:51:09 +0300

    change option.php

--- include/option.php
+++ include/option.php
@@ -1,6 +1,4 @@
 <?php
-    $db = '../db.json'; // path to db file;
+    //$db = '../db.json'; // path to db file;
 
-    //$db = $_SERVER['DOCUMENT_ROOT'] . '/db.json';
-
-    //echo $db;
+    $db = $_SERVER['DOCUMENT_ROOT'] . '/db.json';
